de656574fc61349f5c899ca7c7d29dfe899b05e6
Fadi Aljohari <fadi@fadi-aljohari-linux.scoiumtech.local>
2023-06-15 14:37:51 +0300

    Restructured with Constructor

--- ShippingService_Tests/Systems/Controllers/TestShipmentController.cs
+++ ShippingService_Tests/Systems/Controllers/TestShipmentController.cs
@@ -1,6 +1,6 @@
 using System.Threading.Tasks;
 using AutoMapper;
-using Demo1.TestApi.MockData;
+using ShipmentTestApi.MockData;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
@@ -11,25 +11,33 @@ using ShipmentService.API.Models.Shipment;
 using ShipmentService.API.Validators;
 using Xunit;
 
-namespace Demo1.TestApi.Systems.Controllers
+namespace ShipmentTestApi.Systems.Controllers
 {
     public class TestShipmentController
     {
+
+        private readonly Mock<IShipmentsRepository>? _shipmentRepositoryMock;
+        private readonly Mock<IMapper>? _mapperMock;
+        private readonly Mock<PackageDtoValidator>? _packageDtoValidator;
+
+        public TestShipmentController()
+        {
+            _shipmentRepositoryMock = new Mock<IShipmentsRepository>();
+            _mapperMock = new Mock<IMapper>();
+            _packageDtoValidator = new Mock<PackageDtoValidator>();
+        }
+
+
         [Fact]
         public async Task GetShipments_ShouldReturn200Status()
         {
             // Arrange
-            var shipmentsRepositoryMock = new Mock<IShipmentsRepository>();
-            shipmentsRepositoryMock.Setup(_ => _.GetDetailsAllAsync()).ReturnsAsync(ShipmentMockData.GetShipments());
+            _shipmentRepositoryMock?.Setup(_ => _.GetDetailsAllAsync()).ReturnsAsync(ShipmentMockData.GetShipments());
 
-            var mapperMock = new Mock<IMapper>();
-
-            PackageDtoValidator packageDtoValidator = new PackageDtoValidator();
-
-            var sut = new ShipmentsController(mapperMock.Object, shipmentsRepositoryMock.Object, packageDtoValidator);
+            var sut = new ShipmentsController(_mapperMock?.Object, _shipmentRepositoryMock?.Object, _packageDtoValidator?.Object);
 
             // Act
-            var result = await sut.GetShipments();
+            var result = await sut.GetAllShipments();
 
             // Assert
             result.Result.Should().BeOfType<OkObjectResult>();
@@ -42,18 +50,14 @@ namespace Demo1.TestApi.Systems.Controllers
         {
             // Arrange
             int id = 1;
-            var shipmentsRepositoryMock = new Mock<IShipmentsRepository>();
-            var mapperMock = new Mock<IMapper>();
 
             var shipment = new Shipment { Id = id, ShipmentId = "Fedex" };
-            shipmentsRepositoryMock.Setup(_ => _.GetDetails(id)).ReturnsAsync(shipment);
+            _shipmentRepositoryMock?.Setup(_ => _.GetDetails(id)).ReturnsAsync(shipment);
 
             var expectedDto = new GetShipmentDto { Id = id, ShipmentId = "Fedex" };
-            mapperMock.Setup(mapper => mapper.Map<GetShipmentDto>(shipment)).Returns(expectedDto);
+            _mapperMock?.Setup(mapper => mapper.Map<GetShipmentDto>(shipment)).Returns(expectedDto);
 
-            PackageDtoValidator packageDtoValidator = new PackageDtoValidator();
-
-            var sut = new ShipmentsController(mapperMock.Object, shipmentsRepositoryMock.Object, packageDtoValidator);
+            var sut = new ShipmentsController(_mapperMock?.Object, _shipmentRepositoryMock?.Object, _packageDtoValidator?.Object);
 
             // Act
             var result = await sut.GetShipment(id);
