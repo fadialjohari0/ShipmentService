8cf4faa203caf3667bc1ec11379128e7e0dcfb95
Fadi Aljohari <fadi@fadi-aljohari-linux.scoiumtech.local>
2023-06-15 12:20:15 +0300

    Created New Dto, GetAllPackages

---
+++ ShippingService/Models/Packages/GetAllPackagesDto.cs
@@ -0,0 +1,9 @@
+using ShipmentService.API.Models.Shipment;
+
+namespace ShipmentService.API.Models.Package
+{
+    public class GetAllPackagesDto : BasePackageDto
+    {
+        public int Id { get; set; }
+    }
+}
