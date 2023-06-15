17798b8bba26d1942c9b38f14c5470e1f4164b68
Fadi Aljohari <fadi@fadi-aljohari-linux.scoiumtech.local>
2023-06-15 12:19:52 +0300

    Created BasePackDto

---
+++ ShippingService/Models/Packages/BasePackageDto.cs
@@ -0,0 +1,10 @@
+namespace ShipmentService.API.Models.Shipment
+{
+    public class BasePackageDto
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double Length { get; set; }
+        public double Weight { get; set; }
+    }
+}
