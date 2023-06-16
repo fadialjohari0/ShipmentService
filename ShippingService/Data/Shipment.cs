e072ac1b45540ea16d42a4d95ca03cdb6ac764d5
Fadi Aljohari <fadialjohari0@gmail.com>
2023-06-16 21:59:43 +0300

    Changed Fields name to be more suitable

--- ShippingService/Data/Shipment.cs
+++ ShippingService/Data/Shipment.cs
@@ -3,8 +3,8 @@ namespace ShipmentService.API.Data
     public class Shipment
     {
         public int Id { get; set; }
-        public string ShipmentId { get; set; }
-        public string CarrierServiceId { get; set; }
+        public string ShippingCompany { get; set; }
+        public string ShippingServiceType { get; set; }
 
         public Package Package { get; set; }
     }
