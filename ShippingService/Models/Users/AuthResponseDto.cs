989b49cd9c256b98567e7c0ff242192e4ecafc4c
Fadi Aljohari <fadialjohari0@gmail.com>
2023-06-18 18:14:44 +0300

    Creation of AuthResponseDto

---
+++ ShippingService/Models/Users/AuthResponseDto.cs
@@ -0,0 +1,8 @@
+namespace ShipmentService.API.Models.Users
+{
+    public class AuthResponseDto
+    {
+        public string UserId { get; set; }
+        public string Token { get; set; }
+    }
+}
