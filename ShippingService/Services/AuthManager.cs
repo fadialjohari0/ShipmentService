9feca8849fe983068277e55cacbc3c000246d7db
Fadi Aljohari <fadialjohari0@gmail.com>
2023-06-18 18:18:18 +0300

    Generate Tokens Implementation

--- ShippingService/Services/AuthManager.cs
+++ ShippingService/Services/AuthManager.cs
@@ -1,5 +1,9 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using AutoMapper;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 using ShipmentService.API.Contracts;
 using ShipmentService.API.Data;
 using ShipmentService.API.Models.Users;
@@ -10,29 +14,32 @@ namespace ShipmentService.API.AuthManager
     {
         private readonly IMapper _mapper;
         private readonly UserManager<ApiUser> _userManager;
+        private readonly IConfiguration _configuration;
 
-        public AuthManager(IMapper mapper, UserManager<ApiUser> userManager)
+        public AuthManager(IMapper mapper, UserManager<ApiUser> userManager, IConfiguration configuration)
         {
             this._mapper = mapper;
             this._userManager = userManager;
+            this._configuration = configuration;
         }
 
-        public async Task<bool> Login(LoginDto loginDto)
+        public async Task<AuthResponseDto> Login(LoginDto loginDto)
         {
             var user = await _userManager.FindByEmailAsync(loginDto.Email);
+            bool isValidUser = await _userManager.CheckPasswordAsync(user, loginDto.Password);
 
-            if (user is null)
+            if (user == null || isValidUser == false)
             {
-                return default;
+                return null;
             }
 
-            var isValidCredentials = await _userManager.CheckPasswordAsync(user, loginDto.Password);
+            var token = await GenerateToken(user);
 
-            if (!isValidCredentials)
+            return new AuthResponseDto
             {
-                return default;
-            }
-            return true;
+                Token = token,
+                UserId = user.Id
+            };
         }
 
         public async Task<IEnumerable<IdentityError>> Register(ApiUserDto userDto)
@@ -48,6 +55,33 @@ namespace ShipmentService.API.AuthManager
             }
 
             return result.Errors;
+        }
+
+        private async Task<string> GenerateToken(ApiUser user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtSettings:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var roles = await _userManager.GetRolesAsync(user);
+            var roleClaims = roles.Select(x => new Claim(ClaimTypes.Role, x)).ToList();
+            var userClaims = await _userManager.GetClaimsAsync(user);
+
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+            }
+            .Union(userClaims).Union(roleClaims);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JwtSettings:Issuer"],
+                audience: _configuration["JwtSettings:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(Convert.ToInt32(_configuration["JwtSettings:DurationInMinutes"])),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
