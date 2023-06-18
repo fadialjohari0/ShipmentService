f9870b419966c240c2f31646a67c0af64ac28548
Fadi Aljohari <fadialjohari0@gmail.com>
2023-06-18 15:15:45 +0300

    Creation of Api user Dto

---
+++ ShippingService/Models/Users/ApiUserDto.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ShipmentService.API.Models.Users
+{
+    public class ApiUserDto : LoginDto
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+    }
+}
