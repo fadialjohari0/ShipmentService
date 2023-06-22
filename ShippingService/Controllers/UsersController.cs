d6b068e5df76eb8c309f702a053f435e0f7b0e48
Fadi Aljohari <fadialjohari0@gmail.com>
2023-06-22 16:22:20 +0300

    Added 2 Get Endpoints + Replaced vars

--- ShippingService/Controllers/UsersController.cs
+++ ShippingService/Controllers/UsersController.cs
@@ -1,6 +1,11 @@
+using System.Net;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ShipmentService.API.Contracts;
+using ShipmentService.API.Data;
 using ShipmentService.API.Models.Users;
+using ShipmentService.API.UOW;
 
 namespace ShipmentService.API.Controllers
 {
@@ -8,15 +13,19 @@ namespace ShipmentService.API.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
         private readonly IAuthManager _authManager;
 
-        public UsersController(IAuthManager authManager)
+        public UsersController(IUnitOfWork unitOfWork, IMapper mapper, IAuthManager authManager)
         {
+            this._unitOfWork = unitOfWork;
+            this._mapper = mapper;
             this._authManager = authManager;
         }
 
         [HttpPost]
-        [Route("register")]
+        [Route("Register")]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [ProducesResponseType(StatusCodes.Status200OK)]
@@ -36,13 +45,13 @@ namespace ShipmentService.API.Controllers
         }
 
         [HttpPost]
-        [Route("login")]
+        [Route("Login")]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult> Login([FromBody] LoginDto loginDto)
         {
-            var authResponse = await _authManager.Login(loginDto);
+            AuthResponseDto authResponse = await _authManager.Login(loginDto);
 
             if (authResponse == null)
             {
@@ -53,13 +62,13 @@ namespace ShipmentService.API.Controllers
         }
 
         [HttpPost]
-        [Route("refreshtoken")]
+        [Route("RefreshToken")]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult> RefreshToken([FromBody] AuthResponseDto request)
         {
-            var authResponse = await _authManager.VerifyRefreshToken(request);
+            AuthResponseDto authResponse = await _authManager.VerifyRefreshToken(request);
 
             if (authResponse == null)
             {
@@ -67,6 +76,42 @@ namespace ShipmentService.API.Controllers
             }
 
             return Ok(authResponse);
+        }
+
+        [HttpGet]
+        [Route("GetUsers")]
+        // [Authorize(Roles = "Administrator")]
+        public async Task<ActionResult<IEnumerable<GetUsersDto>>> GetAllUsers()
+        {
+            try
+            {
+                List<ApiUser> users = await _unitOfWork.Users.GetDetailsAllAsync();
+                var record = _mapper.Map<List<GetUsersDto>>(users);
+
+                return Ok(record);
+            }
+            catch (Exception)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, "An error occurred while fetching shipments.");
+            }
+        }
+
+        [HttpGet]
+        [Route("GetUsersShipments")]
+        [Authorize(Roles = "Administrator")]
+        public async Task<ActionResult<IEnumerable<GetUsersDto>>> GetAllUsersWithShipments()
+        {
+            try
+            {
+                List<ApiUser> users = await _unitOfWork.Users.GetDetailsAllAsync();
+                var record = _mapper.Map<List<GetUsersShipmentsDto>>(users);
+
+                return Ok(record);
+            }
+            catch (Exception)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, "An error occurred while fetching shipments.");
+            }
         }
     }
 }
