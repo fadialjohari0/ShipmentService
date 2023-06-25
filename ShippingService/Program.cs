546ac51685d4b1a2863f43f945f9d1df1342b8bb
Fadi Aljohari <fadialjohari0@gmail.com>
2023-06-25 16:21:00 +0300

    Added Access modifier for the integration test

--- ShippingService/Program.cs
+++ ShippingService/Program.cs
@@ -14,77 +14,83 @@ using ShipmentService.API.Repository;
 using ShipmentService.API.UOW;
 using ShipmentService.API.Validators;
 
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-var connectionString = builder.Configuration.GetSection("ConnectionString")["ShippingServiceDbConnectionString"];
-
-builder.Services.AddDbContext<ShipmentServiceDbContext>(options =>
+public class Program
 {
-    options.UseNpgsql(connectionString);
-});
-
-builder.Services.AddIdentityCore<ApiUser>()
-    .AddRoles<IdentityRole>()
-    .AddTokenProvider<DataProtectorTokenProvider<ApiUser>>("ShippingServiceApi")
-    .AddEntityFrameworkStores<ShipmentServiceDbContext>()
-    .AddDefaultTokenProviders();
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-
-builder.Services.AddFluentValidationAutoValidation().AddFluentValidationClientsideAdapters();
-builder.Services.AddValidatorsFromAssemblyContaining<ShipmentDtoValidator>();
-
-builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
-
-builder.Services.AddAutoMapper(typeof(MapperConfig));
-
-builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-builder.Services.AddScoped<IShipmentsRepository, ShipmentRepository>();
-builder.Services.AddScoped<IPackagesRepository, PackageRepository>();
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-builder.Services.AddScoped<IAuthManager, AuthManager>();
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
+    public static void Main(string[] args)
     {
-        ValidateIssuerSigningKey = true,
-        ValidateIssuer = true, //Validate that its from us
-        ValidateAudience = true, //Validate that its from somebody who we recognize
-        ValidateLifetime = true,
-        ClockSkew = TimeSpan.Zero,
-        ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
-        ValidAudience = builder.Configuration["JwtSettings:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF32.GetBytes(builder.Configuration["JwtSettings:Key"])), // i added the key through user-secrets
-    };
-});
 
-var app = builder.Build();
+        var builder = WebApplication.CreateBuilder(args);
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        // Add services to the container.
+
+        var connectionString = builder.Configuration.GetSection("ConnectionString")["ShippingServiceDbConnectionString"];
+
+        builder.Services.AddDbContext<ShipmentServiceDbContext>(options =>
+        {
+            options.UseNpgsql(connectionString);
+        });
+
+        builder.Services.AddIdentityCore<ApiUser>()
+            .AddRoles<IdentityRole>()
+            .AddTokenProvider<DataProtectorTokenProvider<ApiUser>>("ShippingServiceApi")
+            .AddEntityFrameworkStores<ShipmentServiceDbContext>()
+            .AddDefaultTokenProviders();
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+
+        builder.Services.AddFluentValidationAutoValidation().AddFluentValidationClientsideAdapters();
+        builder.Services.AddValidatorsFromAssemblyContaining<ShipmentDtoValidator>();
+
+        builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
+
+        builder.Services.AddAutoMapper(typeof(MapperConfig));
+
+        builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        builder.Services.AddScoped<IShipmentsRepository, ShipmentRepository>();
+        builder.Services.AddScoped<IPackagesRepository, PackageRepository>();
+        builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+        builder.Services.AddScoped<IAuthManager, AuthManager>();
+
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                ValidateIssuer = true, //Validate that its from us
+                ValidateAudience = true, //Validate that its from somebody who we recognize
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero,
+                ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+                ValidAudience = builder.Configuration["JwtSettings:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF32.GetBytes(builder.Configuration["JwtSettings:Key"])), // i added the key through user-secrets
+            };
+        });
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthentication();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
